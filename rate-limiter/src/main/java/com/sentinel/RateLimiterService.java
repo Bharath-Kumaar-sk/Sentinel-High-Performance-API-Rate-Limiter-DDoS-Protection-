2942baf050f7a47e90ed43bf95f57f405268eacc
Bharath-Kumaar-sk <BharathKumaarskbk05@gmail.com>
2026-02-15 10:41:35 +0530

    Add Spring Boot parent dependency and update RateLimiterService with concurrent request handling

--- rate-limiter/src/main/java/com/sentinel/RateLimiterService.java
+++ rate-limiter/src/main/java/com/sentinel/RateLimiterService.java
@@ -6,15 +6,16 @@ public class RateLimiterService {
     private final double MAX_AMOUNT;
     private final double REFILL_RATE;
 
+    //use constructor so in the main funcitonality we can specify the max and refill
     public RateLimiterService(double MAX_AMOUNT, double REFILL_RATE) {
         this.MAX_AMOUNT = MAX_AMOUNT;
         this.REFILL_RATE = REFILL_RATE;
 
     }
 
+    //to make sure we can handle if 2 or more requests come at the same time
     private final ConcurrentHashMap<String, TokenBucket> map = new ConcurrentHashMap<>();
 
-    
     public boolean allowRequest(String Ip) {
         TokenBucket bucket  = map.computeIfAbsent(Ip, k -> new TokenBucket(MAX_AMOUNT, MAX_AMOUNT, System.nanoTime(), REFILL_RATE));
         return bucket.tryConsume();
